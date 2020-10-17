c53e029d1f9eaac394bc471fc9bc27837d3052a1
TishoStefanov84 <tisho.stefanov@abv.bg>
2020-10-17 17:15:54 +0300

    Create simple SoftUniServer SUS

---
+++ SUS.HTTP/IHttpServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Cache;
+using System.Threading.Tasks;
+
+namespace SUS.HTTP
+{
+    public interface IHttpServer
+    {
+        void AddRoute(string path, Func<HttpRequest, HttpResponse> action);
+
+        Task Start(int port);
+    }
+}
