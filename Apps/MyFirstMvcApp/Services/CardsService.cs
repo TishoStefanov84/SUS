80929c86f72dd0a4d4be0fe45b045e1974776e0c
TishoStefanov84 <tisho.stefanov@abv.bg>
2020-11-09 17:49:38 +0200

    Implement data binding

--- Apps/MyFirstMvcApp/Services/CardsService.cs
+++ Apps/MyFirstMvcApp/Services/CardsService.cs
@@ -1,7 +1,15 @@
-﻿namespace MyFirstMvcApp.Services
+﻿using MyFirstMvcApp.Data;
+
+namespace MyFirstMvcApp.Services
 {
     public class CardsService : ICardsService
     {
+        private readonly ApplicationDbContext db;
+
+        public CardsService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
         public void AddCard()
         {
             throw new System.NotImplementedException();
--- SUS.MvcFramework/Host.cs
+++ SUS.MvcFramework/Host.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using SUS.HTTP;
 
@@ -57,19 +58,38 @@ namespace SUS.MvcFramework
                         url = attribute.Url;
                     }
 
-                    routeTable.Add(new Route(url, httpMethod, (request) =>
-                    {
-                        var instace = serviceCollection.CreateInstance(controllerType) as Controller;
-                        instace.Request = request;
-                        var response = method.Invoke(instace, new object[] { }) as HttpResponse;
-                        return response;
-                    }));
+                    routeTable.Add(new Route(url, httpMethod, request => ExecuteAction(request, controllerType, method, serviceCollection)));
 
                     Console.WriteLine(url);
                 }
             }
         }
 
+        private static HttpResponse ExecuteAction(HttpRequest request, Type controllerType, MethodInfo action, IServiceCollection serviceCollection)
+        {
+            var instace = serviceCollection.CreateInstance(controllerType) as Controller;
+            instace.Request = request;
+            var arguments = new List<object>();
+            var parameters = action.GetParameters();
+            foreach (var parameter in parameters)
+            {
+                var parameterValue = GetParameterFromRequest(request, parameter.Name);
+                arguments.Add(parameterValue);
+            }
+
+            var response = action.Invoke(instace, arguments.ToArray()) as HttpResponse;
+            return response;
+        }
+
+        private static string GetParameterFromRequest(HttpRequest request, string parameterName)
+        {
+            if (request.FormData.ContainsKey(parameterName))
+            {
+                return request.FormData[parameterName];
+            }
+
+            return null;
+        }
         private static void AutoRegisterStaticFile(List<Route> routeTable)
         {
             var staticFiles = Directory.GetFiles("wwwroot", "*", SearchOption.AllDirectories);
